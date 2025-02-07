a0686ee7418514fbc91e3235714434d99b412197
Norman Xu <hi@norm.im>
2025-02-07 15:03:53 +0000

    fix: deps

--- packages/sdk/eslint.config.js
+++ packages/sdk/eslint.config.js
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from "@ecp.eth/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
