7bc86d95f737257e0628b49f6e1878288bc6ed36
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2025-02-13 12:26:12 +0200

    fix(indexer): use strong ponder strong ordering guarantee

--- apps/indexer/src/index.ts
+++ apps/indexer/src/index.ts
@@ -6,7 +6,7 @@ ponder.on("CommentsV1:CommentAdded", async ({ event, context }) => {
   let normalizedTargetUri =
     event.args.commentData.targetUri.trim().length > 0
       ? event.args.commentData.targetUri
-      : null;
+      : "";
 
   try {
     const urlObj = new URL(event.args.commentData.targetUri);
@@ -15,38 +15,39 @@ ponder.on("CommentsV1:CommentAdded", async ({ event, context }) => {
     console.error(error);
   }
 
-  await context.db
-    .insert(schema.comment)
-    .values({
-      id: event.args.commentId,
-      content: event.args.commentData.content,
-      metadata: event.args.commentData.metadata,
-      targetUri: normalizedTargetUri,
-      parentId:
-        event.args.commentData.parentId !==
-        "0x0000000000000000000000000000000000000000000000000000000000000000" // bytes32(0)
-          ? event.args.commentData.parentId
-          : null,
-      author: event.args.commentData.author,
-      txHash: event.transaction.hash,
-      timestamp: new Date(Number(event.block.timestamp) * 1000),
-      chainId: context.network.chainId,
-      appSigner: event.args.commentData.appSigner,
-      logIndex: event.log.logIndex,
-    })
-    .onConflictDoNothing();
+  await context.db.insert(schema.comment).values({
+    id: event.args.commentId,
+    content: event.args.commentData.content,
+    metadata: event.args.commentData.metadata,
+    targetUri: normalizedTargetUri,
+    parentId:
+      event.args.commentData.parentId !==
+      "0x0000000000000000000000000000000000000000000000000000000000000000" // bytes32(0)
+        ? event.args.commentData.parentId
+        : null,
+    author: event.args.commentData.author,
+    txHash: event.transaction.hash,
+    timestamp: new Date(Number(event.block.timestamp) * 1000),
+    chainId: context.network.chainId,
+    appSigner: event.args.commentData.appSigner,
+    logIndex: event.log.logIndex,
+  });
 });
 
 ponder.on("CommentsV1:CommentDeleted", async ({ event, context }) => {
-  await context.db
-    .insert(schema.comment)
-    .values({
-      id: event.args.commentId,
-      deletedAt: new Date(Number(event.block.timestamp) * 1000),
-    })
-    .onConflictDoUpdate({
-      deletedAt: new Date(Number(event.block.timestamp) * 1000),
-    });
+  const existingComment = await context.db.find(schema.comment, {
+    id: event.args.commentId,
+  });
+
+  if (existingComment && existingComment.author === event.args.author) {
+    await context.db
+      .update(schema.comment, {
+        id: event.args.commentId,
+      })
+      .set({
+        deletedAt: new Date(Number(event.block.timestamp) * 1000),
+      });
+  }
 });
 
 ponder.on("CommentsV1:ApprovalAdded", async ({ event, context }) => {
@@ -73,15 +74,10 @@ ponder.on("CommentsV1:ApprovalRemoved", async ({ event, context }) => {
   const id = `${event.args.author}-${event.args.appSigner}-${context.network.chainId}`;
 
   await context.db
-    .insert(schema.approvals)
-    .values({
+    .update(schema.approvals, {
       id,
-      author: event.args.author,
-      appSigner: event.args.appSigner,
-      chainId: context.network.chainId,
-      deletedAt: new Date(Number(event.block.timestamp) * 1000),
     })
-    .onConflictDoUpdate({
+    .set({
       deletedAt: new Date(Number(event.block.timestamp) * 1000),
     });
 });
