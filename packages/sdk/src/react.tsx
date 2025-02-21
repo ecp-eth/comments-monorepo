440535dfc217830fe297a483e30ff1e03185d349
Norman Xu <hi@norm.im>
2025-02-21 22:41:57 +0800

    feat: add CommentsEmbed to docs

--- packages/sdk/src/react.tsx
+++ packages/sdk/src/react.tsx
@@ -9,10 +9,12 @@ import { Hex, SignTypedDataParameters } from "viem";
 import { useSignTypedData } from "wagmi";
 import { COMMENTS_EMBED_DEFAULT_URL } from "./constants.js";
 import { EmbedConfigSchema, type EmbedConfigSchemaType } from "./schemas.js";
-import { compressToURI } from "lz-ts";
+import lz from "lz-ts";
 
 // also export the type for generating docs correctly
 export type { EmbedConfigSchemaType } from "./schemas.js";
+
+const { compressToURI } = lz;
 
 /**
  * A hook for repeat gasless transaction pattern
