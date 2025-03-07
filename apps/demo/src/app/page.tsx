d18b3cb91427ba3b3e960f351504e118dbcdc3e8
Michal Kvasničák <michal.kvasnicak@gmail.com>
2025-03-07 09:04:14 +0100

    feat: doc and source code links (#74)

--- apps/demo/src/app/page.tsx
+++ apps/demo/src/app/page.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { useAccount } from "wagmi";
 import { CommentSection } from "../components/comments/CommentSection";
 import { CommentSectionGasless } from "../components/comments/gasless/CommentSectionGasless";
+import Link from "next/link";
 
 export default function Home() {
   const { address } = useAccount();
@@ -14,6 +15,20 @@ export default function Home() {
 
   return (
     <main className="min-h-screen bg-gray-100 py-8">
+      <div className="max-w-4xl mx-auto flex justify-end p-2 gap-2">
+        <Link
+          className="text-sm font-semibold"
+          href="https://docs.ethcomments.xyz"
+        >
+          Documentation
+        </Link>
+        <Link
+          className="text-sm font-semibold"
+          href="https://github.com/ecp-eth/comments-monorepo/tree/main/apps/demo"
+        >
+          Github
+        </Link>
+      </div>
       <div className="max-w-4xl mx-auto bg-white p-8 rounded-lg shadow">
         <div className="flex justify-between items-center mb-6">
           <h1 className="text-2xl font-bold">Comments</h1>
