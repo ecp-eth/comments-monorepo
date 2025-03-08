0d7b5461d6d961712fabdcfde634d0061a85775b
Norman Xu <hi@norm.im>
2025-03-08 17:01:02 +0800

    fix: author box is missing from gasless comment box

---
+++ apps/demo/src/components/comments/CommentBoxAuthor.tsx
@@ -0,0 +1,20 @@
+import useEnrichedAuthor from "@/hooks/useEnrichedAuthor";
+import { AuthorType } from "@/lib/types";
+import { CommentAuthorAvatar } from "./CommentAuthorAvatar";
+import { getCommentAuthorNameOrAddress } from "./helpers";
+
+export function CommentBoxAuthor(author: AuthorType) {
+  const enrichedAuthor = useEnrichedAuthor(author);
+
+  return (
+    <div
+      className="flex flex-row gap-2 items-center"
+      title={`Publishing as ${getCommentAuthorNameOrAddress(enrichedAuthor)}`}
+    >
+      <CommentAuthorAvatar author={enrichedAuthor} />
+      <div className="text-xs text-gray-500">
+        {getCommentAuthorNameOrAddress(enrichedAuthor)}
+      </div>
+    </div>
+  );
+}
