ee8ce44bfba6c2e0ecac180966b569dbc66a7332
Michal Kvasničák <michal.kvasnicak@brosoft.sk>
2025-03-21 15:55:23 +0100

    chore: revert with error

--- packages/test-protocol/src/BatchCallDelegationProtocol.sol
+++ packages/test-protocol/src/BatchCallDelegationProtocol.sol
@@ -11,8 +11,19 @@ contract BatchCallDelegation {
   function execute(Call[] calldata calls) external payable {
     for (uint256 i = 0; i < calls.length; i++) {
       Call memory call = calls[i];
-      (bool success, ) = call.to.call{value: call.value}(call.data);
-      require(success, "call reverted");
+      (bool success, bytes memory result) = call.to.call{value: call.value}(call.data);
+
+      if (!success) {
+        // If the result length is less than 68, then the transaction failed silently
+        if (result.length < 4) {
+          revert(string(abi.encodePacked("Call ", i, " reverted without reason")));
+        }
+        assembly {
+          // Skip the first 4 bytes of the result which contain the error signature
+          result := add(result, 0x04)
+        }
+        revert(string(abi.encodePacked("Call ", i, " reverted: ", abi.decode(result, (string)))));
+      }
     }
   }
 }
