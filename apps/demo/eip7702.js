3076a4467c986e0ad771be8ead426518ef2711c1
Michal Kvasničák <michal.kvasnicak@brosoft.sk>
2025-03-21 15:04:22 +0100

    chore: test script

---
+++ apps/demo/eip7702.js
@@ -0,0 +1,121 @@
+import {
+  createWalletClient,
+  encodeFunctionData,
+  http,
+  parseAbi,
+  createPublicClient,
+  parseEther,
+} from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { anvil } from "viem/chains";
+import { eip7702Actions } from "viem/experimental";
+import { CommentsV1Abi } from "@ecp.eth/sdk/abis";
+import {
+  COMMENTS_V1_ADDRESS,
+  createCommentData,
+  createCommentTypedData,
+} from "@ecp.eth/sdk";
+
+const YOUR_PRIVATE_KEY = "0x";
+const BATCH_CALL_DELEGATION_CONTRACT_ADDRESS = "0x";
+const YOINK_CONTRACT_ADDRESS = "0x";
+
+const BATCH_CALL_DELEGATION_CONTRACT_ABI = [
+  {
+    type: "function",
+    name: "execute",
+    inputs: [
+      {
+        name: "calls",
+        type: "tuple[]",
+        components: [
+          {
+            name: "data",
+            type: "bytes",
+          },
+          {
+            name: "to",
+            type: "address",
+          },
+          {
+            name: "value",
+            type: "uint256",
+          },
+        ],
+      },
+    ],
+    outputs: [],
+    stateMutability: "payable",
+  },
+];
+
+const account = privateKeyToAccount(YOUR_PRIVATE_KEY);
+const signerAccount = privateKeyToAccount(APP_SIGNER_PRIVATE_KEY);
+const client = createPublicClient({
+  chain: anvil,
+  transport: http(),
+});
+
+const walletClient = createWalletClient({
+  account,
+  chain: anvil,
+  transport: http(),
+}).extend(eip7702Actions());
+
+const authorization = await walletClient.signAuthorization({
+  account,
+  contractAddress: BATCH_CALL_DELEGATION_CONTRACT_ADDRESS,
+});
+
+console.log(authorization);
+
+const commentData = createCommentData({
+  content: "Hello World!",
+  targetUri: "http://localhost:3000",
+  parentId: undefined,
+  author: account.address,
+  appSigner: signerAccount.address,
+});
+
+const typedCommentData = createCommentTypedData({
+  commentData,
+  chainId: anvil.id,
+});
+
+const appSignature = await account.signTypedData(typedCommentData);
+
+const tx = await walletClient.writeContract({
+  address: account.address,
+  abi: BATCH_CALL_DELEGATION_CONTRACT_ABI,
+  functionName: "execute",
+  args: [
+    [
+      {
+        data: encodeFunctionData({
+          abi: parseAbi(["function yoink()"]),
+          functionName: "yoink",
+          args: [],
+        }),
+        to: YOINK_CONTRACT_ADDRESS,
+        value: 0n,
+      },
+      {
+        data: encodeFunctionData({
+          abi: CommentsV1Abi,
+          functionName: "postCommentAsAuthor",
+          args: [commentData, appSignature],
+        }),
+        to: COMMENTS_V1_ADDRESS,
+        value: 0n,
+      },
+    ],
+  ],
+  // gas here must be somehow computed because it can't compute it automatically
+  gas: parseEther("0.00000000001"),
+});
+
+console.log(tx);
+
+const receipt = await client.waitForTransactionReceipt({ hash: tx });
+
+console.log(receipt);
