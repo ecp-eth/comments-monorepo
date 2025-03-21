04b3b26f0540f4cc6d9f0c20af21900c009e7ffb
Michal Kvasničák <michal.kvasnicak@brosoft.sk>
2025-03-21 07:05:16 +0100

    fix: retry processing block in case of malformed rpc result

---
+++ apps/indexer/src/lib/try-async.ts
@@ -0,0 +1,30 @@
+export async function tryAsync<T>(
+  fn: () => Promise<T>,
+  {
+    onError,
+    retries = 10,
+  }: {
+    onError: (error: unknown) => void | Promise<void>;
+    /**
+     * @default 10
+     */
+    retries?: number;
+  }
+) {
+  for (let i = 0; i < retries; i++) {
+    try {
+      return await fn();
+    } catch (error) {
+      console.error(`[tryAsync] error ${i + 1} of ${retries}`);
+      console.error(error);
+
+      await onError(error);
+
+      if (i === retries - 1) {
+        throw error;
+      } else {
+        console.log(`[tryAsync] retrying...`);
+      }
+    }
+  }
+}
