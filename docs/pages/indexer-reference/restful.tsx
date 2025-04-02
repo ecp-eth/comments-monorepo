c87946c3f65255a8ecc2131e455ac72b4dc539c8
Norm X <hi@norm.im>
2025-04-02 21:02:34 +0800

    fix: add type check to expo and docs (#189)

--- docs/pages/indexer-reference/restful.tsx
+++ docs/pages/indexer-reference/restful.tsx
@@ -1,23 +1,21 @@
-import React, { PropsWithChildren } from 'react'
+import React, { PropsWithChildren } from "react";
 
-import SwaggerUI from "swagger-ui-react"
-import "swagger-ui-react/swagger-ui.css"
-import "./custom-swagger-override.css"
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+import "./custom-swagger-override.css";
 
 function Heading5({ children }: PropsWithChildren) {
-  return <h5 className="vocs_H5 vocs_Heading">{children}</h5>
+  return <h5 className="vocs_H5 vocs_Heading">{children}</h5>;
 }
 
 function HorizontalRule() {
-  return <hr className="vocs_HorizontalRule" />
+  return <hr className="vocs_HorizontalRule" />;
 }
 
 export default function RestAPIDoc() {
   return (
     <div className="rest-api-doc">
-      <Heading5>
-        @ecp.eth/indexer
-      </Heading5>
+      <Heading5>@ecp.eth/indexer</Heading5>
       <HorizontalRule />
       <SwaggerUI
         url="/indexer-openapi.yaml"
@@ -25,5 +23,5 @@ export default function RestAPIDoc() {
         tryItOutEnabled={false}
       />
     </div>
-  )
+  );
 }
