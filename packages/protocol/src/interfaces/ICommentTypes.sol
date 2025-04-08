1841e665744de9e8b16523393cc137fe757da062
David Furlong <dvfurlong@googlemail.com>
2025-04-08 17:44:14 +0200

    fix: remove parentId and threadId and any validation, to support chain agnostic comment storage

--- packages/protocol/src/interfaces/ICommentTypes.sol
+++ packages/protocol/src/interfaces/ICommentTypes.sol
@@ -5,9 +5,7 @@ interface ICommentTypes {
     /// @notice Struct containing all data for a comment
     /// @param content The actual text content of the comment
     /// @param metadata Additional JSON metadata for the comment
-    /// @param targetUri the URI about which the comment is being made
-    /// @param parentId The ID of the parent comment if this is a reply, otherwise bytes32(0)
-    /// @param threadId The ID of the thread this comment belongs to
+    /// @param targetUri the URI about which the comment is being made, for replies uses CAIP-10 format: eip155:${chainId}/${contractAddress}/${commentId}
     /// @param author The address of the comment author
     /// @param appSigner The address of the application signer that authorized this comment
     /// @param deadline Timestamp after which the signatures for this comment become invalid
@@ -16,8 +14,6 @@ interface ICommentTypes {
         string content;
         string metadata;
         string targetUri;
-        bytes32 parentId;
-        bytes32 threadId;
         address author;
         address appSigner;
         uint256 nonce;
