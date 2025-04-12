25ebebe18cb8858ce4b5eb720e7f8202a4932c1a
Norman Xu <hi@norm.im>
2025-04-12 22:06:34 +0100

    chore: update metadata comments

--- packages/protocol/src/interfaces/ICommentTypes.sol
+++ packages/protocol/src/interfaces/ICommentTypes.sol
@@ -5,14 +5,14 @@ pragma solidity ^0.8.20;
 interface ICommentTypes {
     /// @notice Struct containing all comment data
     /// @param content The actual text content of the comment
-    /// @param metadata Additional JSON metadata for the comment
+    /// @param metadata Additional JSON data that shouldn't be shown to the user as it is
     /// @param targetUri the URI about which the comment is being made, for replies uses an extended CAIP-2 format: eip155:${chainId}/${contractAddress}/${commentId}
+    /// @param commentType The type of the comment (e.g. "question", "answer", "feedback", etc.)
     /// @param author The address of the comment author
     /// @param appSigner The address of the application signer that authorized this comment
-    /// @param deadline Timestamp after which the signatures for this comment become invalid
-    /// @param nonce The nonce for the comment
     /// @param channelId The channel ID associated with the comment
-    /// @param commentType The type of the comment (e.g. "question", "answer", "feedback", etc.)
+    /// @param nonce The nonce for the comment
+    /// @param deadline Timestamp after which the signatures for this comment become invalid
     struct CommentData {
         string content;
         string metadata;
