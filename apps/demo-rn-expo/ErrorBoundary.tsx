e1c00affa86b34ee8255c99dc799dcebcd09c0d8
Norman Xu <hi@norm.im>
2025-04-12 22:29:23 +0100

    chore: update RN demo libs

--- apps/demo-rn-expo/ErrorBoundary.tsx
+++ apps/demo-rn-expo/ErrorBoundary.tsx
@@ -1,6 +1,7 @@
 import React, { Component, ErrorInfo, ReactNode } from "react";
-import { SafeAreaView, Text, View, DevSettings } from "react-native";
+import { Text, View, DevSettings } from "react-native";
 import Button from "./ui/Button";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 interface Props {
   children: ReactNode;
