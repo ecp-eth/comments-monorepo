4c4588abf5a1483a31c5a30afd86de7deee50231
Norman Xu <hi@norm.im>
2025-04-12 22:29:23 +0100

    chore: update RN demo libs

--- apps/demo-rn-expo/eslint.config.js
+++ apps/demo-rn-expo/eslint.config.js
@@ -1,4 +1,12 @@
 import { config } from "@ecp.eth/eslint-config/react-internal";
+import globals from "globals";
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default [...config, {
+  files: ["metro.config.cjs"],
+  languageOptions: {
+    globals: {
+      ...globals.node
+    }
+  }
+}];
