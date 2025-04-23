de81a30803bc2b3eecb456d262810fce522a6151
Michal Kvasničák <michal.kvasnicak@brosoft.sk>
2025-04-23 12:44:35 +0200

    chore: add missing function

--- packages/sdk/src/comments/types.ts
+++ packages/sdk/src/comments/types.ts
@@ -4,6 +4,54 @@ import type { CommentsV1Abi } from "../abis.js";
 
 export type CommentsV1AbiType = typeof CommentsV1Abi;
 
+/**
+ * The data structure of a comment to be signed and then passed to the Embed API
+ */
+export type CommentData = {
+  /**
+   * The content of the comment
+   */
+  content: string;
+  /**
+   * Metadata about the comment
+   */
+  metadata: string;
+  /**
+   * Empty string for replies
+   */
+  targetUri: string;
+  /**
+   * The type of the comment
+   */
+  commentType: string;
+  /**
+   * The ID of the channel
+   */
+  channelId: bigint;
+  /**
+   * id of parent comments if it has one, 0x for no parent comment
+   *
+   * @remarks This zero address (32 bytes of zeros) indicates the comment has no parent and is a top-level comment
+   */
+  parentId: Hex;
+  /**
+   * The address of the author of the comment
+   */
+  author: Hex;
+  /**
+   * The address of the app signer
+   */
+  appSigner: Hex;
+  /**
+   * The nonce for the user per app
+   */
+  nonce: bigint;
+  /**
+   * The deadline of the comment submission in seconds since epoch
+   */
+  deadline: bigint;
+};
+
 export type ContractWriteFunctions = {
   postComment: (
     args: ContractFunctionParameters<
