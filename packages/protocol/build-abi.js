1944a8a10b62628475885e3d18f5c15a6830c32c
Norman Xu <hi@norm.im>
2025-04-24 13:43:22 +0100

    fix(protocol): add Channel ABIs to script

--- packages/protocol/build-abi.js
+++ packages/protocol/build-abi.js
@@ -12,9 +12,18 @@ const outputAbiPaths = [resolve(
   "../../packages/sdk/src/abis.ts"
 )];
 
-const abi = execFileSync(
+const commentV1ABI = execFileSync(
   "pnpm",
   ["forge", "inspect", "./src/CommentsV1.sol:CommentsV1", "abi", "--json"],
+  {
+    cwd: currentDir,
+    encoding: "utf-8",
+  }
+);
+
+const channelManagerABI = execFileSync(
+  "pnpm",
+  ["forge", "inspect", "./src/ChannelManager.sol:ChannelManager", "abi", "--json"],
   {
     cwd: currentDir,
     encoding: "utf-8",
@@ -26,7 +35,12 @@ const formattedAbi = await format(
   /**
    * ABI of the CommentsV1 contract.
    */
-  export const CommentsV1Abi = ${abi.trim()} as const;
+  export const CommentsV1Abi = ${commentV1ABI.trim()} as const;
+
+  /**
+   * ABI of the ChannelManager contract.
+   */
+  export const ChannelManagerAbi = ${channelManagerABI.trim()} as const;
 `,
   { parser: "typescript" }
 );
