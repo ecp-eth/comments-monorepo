becaaf8846b0d245ae9f4d40911764195ecea785
Michal Kvasničák <michal.kvasnicak@brosoft.sk>
2025-05-12 16:04:55 +0200

    chore: document

--- packages/protocol/src/libraries/Comments.sol
+++ packages/protocol/src/libraries/Comments.sol
@@ -14,6 +14,8 @@ library Comments {
     /// @param nonce The nonce for the comment
     /// @param deadline Timestamp after which the signatures for this comment become invalid
     /// @param parentId The ID of the parent comment if this is a reply, otherwise bytes32(0)
+    /// @param createdAt The timestamp when the comment was created
+    /// @param updatedAt The timestamp when the comment was last updated
     struct CommentData {
         // Pack these two addresses together (saves 1 storage slot)
         address author; // 20 bytes
@@ -32,6 +34,17 @@ library Comments {
         string commentType;
     }
 
+    /// @notice Struct containing all comment data for creating a comment
+    /// @param author The address of the comment author
+    /// @param app The address of the application signer that authorized this comment
+    /// @param channelId The channel ID associated with the comment
+    /// @param nonce The nonce for the comment
+    /// @param deadline Timestamp after which the signatures for this comment become invalid
+    /// @param parentId The ID of the parent comment if this is a reply, otherwise bytes32(0)
+    /// @param content The actual text content of the comment
+    /// @param metadata Additional JSON data that shouldn't be shown to the user as it is
+    /// @param targetUri the URI about which the comment is being made
+    /// @param commentType The type of the comment (e.g. "question", "answer", "feedback", etc.)
     struct CreateCommentData {
         address author;
         address app;
