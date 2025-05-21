fea85f3bd4d8f5dc9fb79e9d624f1d0509a1dc6a
Michal Kvasničák <michal.kvasnicak@gmail.com>
2025-05-21 10:32:14 +0200

    refactor: spread struct properties in events (#287)

--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -5,7 +5,7 @@ import { toHex } from "viem/utils";
  * It is created using the CREATE2 opcode so should be identical across chains if no collisions occur.
  */
 export const COMMENT_MANAGER_ADDRESS =
-  "0x919D7C6c4753f634f490e7D5D642336488bba888" as const;
+  "0x25F8031c7991f6e41E9098B2Bbc535Abd55e114a" as const;
 
 /**
  * The address of the ChannelManager contract.
