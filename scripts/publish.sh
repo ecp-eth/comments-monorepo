a617b5d4a7f9a20691c9e5cef80fdaa5b9c1143c
Norman Xu <hi@norm.im>
2025-06-09 11:22:54 +0100

    fix: cleaner way to line break

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -60,7 +60,7 @@ echo "${GREEN}🔔🔔🔔 Preparation Completed 🔔🔔🔔${NC}"
 echo ""
 echo "Now please ${YELLOW}👀 review the changes${NC}, then come back here to continue."
 read -p "Reviewed? (y/N)" -n 1 -r
-echo ""
+echo
 
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     echo "${RED}❌ Aborting publish${NC}"
