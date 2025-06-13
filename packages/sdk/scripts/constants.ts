afd729a32ef2347976a41f7187aa9f7681f3618e
David Furlong <dvfurlong@googlemail.com>
2025-06-13 14:42:46 +0200

    fix nft server

--- packages/sdk/scripts/constants.ts
+++ packages/sdk/scripts/constants.ts
@@ -2,14 +2,16 @@
  * The address of the ChannelManager contract on the dev chain
  */
 export const CHANNEL_MANAGER_ADDRESS =
-  "0x7950f5fEb11687bdA31341E956e299850Fc9594E";
+  "0x7950f5fEb11687bdA31341E956e299850Fc9594E" as const;
 
 /**
  * The address of the Comments contract on the dev chain
  */
-export const COMMENTS_ADDRESS = "0x65673E54Eea755D6aE7EBD523125886a1342316a";
+export const COMMENTS_ADDRESS =
+  "0x65673E54Eea755D6aE7EBD523125886a1342316a" as const;
 
 /**
  * The address of the NoopHook contract on the dev chain
  */
-export const NOOP_HOOK_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const NOOP_HOOK_ADDRESS =
+  "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512" as const;
