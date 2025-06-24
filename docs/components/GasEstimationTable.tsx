6e21a3ea96be14e7cfa30d9b64f5aaa1bf2f3b0a
Norm X <hi@norm.im>
2025-06-24 11:04:24 +0100

    fix: format currency in dollar (#401)

--- docs/components/GasEstimationTable.tsx
+++ docs/components/GasEstimationTable.tsx
@@ -1,6 +1,6 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import Decimal from "decimal.js";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 type GasEstimationTableProps = {
   operations: {
@@ -15,50 +15,65 @@ export function GasEstimationTable({
   gweiPerGases,
 }: GasEstimationTableProps) {
   const [usdPerEth, setUsdPerEth] = useState(undefined);
-
+  const formatter = useMemo(
+    () =>
+      new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        maximumSignificantDigits: 2,
+      }),
+    [],
+  );
   useEffect(() => {
     getETHPrice().then(setUsdPerEth);
   }, []);
 
   return (
-    <table className="vocs_Table">
-      <thead>
-        <tr className="vocs_TableRow">
-          <th className="vocs_TableHeader">Operation</th>
-          <th className="vocs_TableHeader">Gas Units</th>
-          {gweiPerGases.map((price, index) => (
-            <th key={index} className="vocs_TableHeader">
-              at {price} gwei
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {operations.map((operation) => (
-          <tr key={operation.name} className="vocs_TableRow">
-            <td className="vocs_TableCell">{operation.name}</td>
-            <td className="vocs_TableCell">{operation.gasUnits}</td>
-            {gweiPerGases.map((gweiPerGasString, index) => {
-              const gweiPerGas = new Decimal(gweiPerGasString);
-              return (
-                <td key={index} className="vocs_TableCell">
-                  {usdPerEth == null ? (
-                    <Skeleton className="w-full h-[20px] rounded-full" />
-                  ) : (
-                    `${gweiPerGas
-                      .mul(operation.gasUnits)
-                      .div(1000000000)
-                      .mul(usdPerEth)
-                      .toSignificantDigits(2)
-                      .toString()}`
-                  )}
-                </td>
-              );
-            })}
+    <>
+      <table className="vocs_Table">
+        <thead>
+          <tr className="vocs_TableRow">
+            <th className="vocs_TableHeader">Operation</th>
+            <th className="vocs_TableHeader">Gas Units</th>
+            {gweiPerGases.map((price, index) => (
+              <th key={index} className="vocs_TableHeader">
+                at {price} gwei
+              </th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {operations.map((operation) => (
+            <tr key={operation.name} className="vocs_TableRow">
+              <td className="vocs_TableCell">{operation.name}</td>
+              <td className="vocs_TableCell">{operation.gasUnits}</td>
+              {gweiPerGases.map((gweiPerGasString, index) => {
+                const gweiPerGas = new Decimal(gweiPerGasString);
+                return (
+                  <td key={index} className="vocs_TableCell">
+                    {usdPerEth == null ? (
+                      <Skeleton className="w-full h-[20px] rounded-full" />
+                    ) : (
+                      `${formatter.format(
+                        gweiPerGas
+                          .mul(operation.gasUnits)
+                          .div(1000000000)
+                          .mul(usdPerEth)
+                          .toNumber(),
+                      )}`
+                    )}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p className="text-xs italic">
+        All prices are shown in U.S. dollars, based on real-time data from
+        CoinGecko.com.
+      </p>
+    </>
   );
 }
 
