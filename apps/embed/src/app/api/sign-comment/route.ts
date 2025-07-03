e8b531af7cca201cad23954fe60c6a2b7bf47dfc
Norman Xu <hi@norm.im>
2025-07-03 11:06:31 +0100

    fix: according to PR comments

--- apps/embed/src/app/api/sign-comment/route.ts
+++ apps/embed/src/app/api/sign-comment/route.ts
@@ -35,7 +35,7 @@ export async function POST(req: Request) {
   if (content.length > env.COMMENT_CONTENT_LENGTH_LIMIT) {
     return Response.json(
       {
-        error: "Comment content too large",
+        error: "Comment content length limit exceeded",
       },
       { status: 413 },
     );
