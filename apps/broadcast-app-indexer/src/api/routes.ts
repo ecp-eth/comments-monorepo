90303f205aecca47abb45806a3fd7c1b05e4c1fe
michalkvasnicak <michal.kvasnicak@gmail.com>
2025-07-22 09:56:30 +0200

    refactor(broadcast-app-indexer): streamline startBlock assignment and rename channel route initialization function

--- apps/broadcast-app-indexer/src/api/routes.ts
+++ apps/broadcast-app-indexer/src/api/routes.ts
@@ -1,6 +1,6 @@
 import type { OpenAPIHono } from "@hono/zod-openapi";
-import { initializeChannelRoutes } from "./channels/routes";
+import { initializeChannelsRoutes } from "./channels/routes";
 
 export async function initializeRoutes(api: OpenAPIHono): Promise<void> {
-  await initializeChannelRoutes(api);
+  await initializeChannelsRoutes(api);
 }
