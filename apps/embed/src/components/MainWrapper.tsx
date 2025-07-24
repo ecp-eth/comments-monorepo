8e3ec45fccde839365ae94278516020389aeca65
Norman Xu <hi@norm.im>
2025-07-24 10:41:54 +0100

    fix: comments

--- apps/embed/src/components/MainWrapper.tsx
+++ apps/embed/src/components/MainWrapper.tsx
@@ -1,10 +1,12 @@
 import { cn } from "@ecp.eth/shared/helpers";
 
 /**
- * This component share the stying of the wrapper of 3 different pages:
+ * This component shares the stying of the wrapper of 3 different pages:
  * - /
  * - /by-author
  * - /by-replies
+ *
+ * The idea having the component is to avoid code duplication which may introduce inconsistencies overtime.
  */
 export function MainWrapper({
   children,
