c7c2207314fdb14cc0fcd4db661ecf73c8a36f98
michalkvasnicak <michal.kvasnicak@gmail.com>
2025-07-28 15:55:52 +0200

    chore: add editor composer

--- apps/broadcast-app/src/env/server.ts
+++ apps/broadcast-app/src/env/server.ts
@@ -1,3 +1,4 @@
+import { HexSchema } from "@ecp.eth/sdk/core/schemas";
 import { z } from "zod";
 
 const serverEnvSchema = z.object({
@@ -11,6 +12,10 @@ const serverEnvSchema = z.object({
   FARCASTER_MINI_APP_NAME: z.string().nonempty().max(32),
   FARCASTER_MINI_APP_WEBHOOK_URL: z.string().url(),
   FARCASTER_MINI_APP_URL: z.string().url(),
+  PINATA_JWT: z.string().nonempty(),
+  PRIVATE_RPC_URL: z.string().url(),
+  COMMENT_CONTENT_LENGTH_LIMIT: z.number().int().positive().default(10240),
+  APP_SIGNER_PRIVATE_KEY: HexSchema,
 });
 
 const result = serverEnvSchema.safeParse(process.env);
--- apps/broadcast-app/src/wagmi/config.ts
+++ apps/broadcast-app/src/wagmi/config.ts
@@ -1,11 +1,14 @@
 import { http, createConfig } from "wagmi";
 import { base } from "wagmi/chains";
 import { farcasterMiniApp as miniAppConnector } from "@farcaster/miniapp-wagmi-connector";
+import { publicEnv } from "@/env/public";
+
+export const chain = base;
 
 export const config = createConfig({
-  chains: [base],
+  chains: [chain],
   transports: {
-    [base.id]: http(),
+    [chain.id]: http(publicEnv.NEXT_PUBLIC_RPC_URL),
   },
   connectors: [miniAppConnector()],
 });
