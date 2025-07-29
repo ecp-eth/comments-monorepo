7a68fccdb0cd30ed2d5d812f12476ceeeb5d6530
michalkvasnicak <michal.kvasnicak@gmail.com>
2025-07-29 16:16:16 +0200

    feat(broadcast-app): log generated private key and corresponding address in app signer script

--- apps/broadcast-app/scripts/generate-app-signer.ts
+++ apps/broadcast-app/scripts/generate-app-signer.ts
@@ -8,9 +8,10 @@
  * ./generate-app-signer.ts
  * ```
  */
-import { generatePrivateKey } from "viem/accounts";
+import { generatePrivateKey, privateKeyToAddress } from "viem/accounts";
 
 const privateKey = generatePrivateKey();
 
 console.log("Private key: ", privateKey);
+console.log("Address: ", privateKeyToAddress(privateKey));
 process.exit(0);
