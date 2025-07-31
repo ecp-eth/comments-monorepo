c16f55986e28b38dffa675f813f541ce5d0d7eb3
michalkvasnicak <michal.kvasnicak@gmail.com>
2025-07-31 09:34:08 +0200

    fix(broadcast-app): adjust maximum replies and comments per page for improved performance

--- apps/broadcast-app/src/constants.ts
+++ apps/broadcast-app/src/constants.ts
@@ -1,2 +1,3 @@
 export const TX_RECEIPT_TIMEOUT = 1000 * 60 * 2; // 2 minutes
-export const MAX_INITIAL_REPLIES_ON_PARENT_COMMENT = 3;
+export const MAX_INITIAL_REPLIES_ON_PARENT_COMMENT = 2;
+export const MAX_COMMENTS_PER_PAGE = 20;
