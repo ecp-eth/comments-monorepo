110761473e6ce4f542bbe68c794e339f1778d228
michalkvasnicak <michal.kvasnicak@gmail.com>
2025-07-31 09:18:57 +0200

    refactor(broadcast-app): streamline channel subscription and notification management with new hooks and components

--- apps/broadcast-app/src/hooks/useSubscribeToChannel.ts
+++ apps/broadcast-app/src/hooks/useSubscribeToChannel.ts
@@ -1,8 +1,12 @@
 import { publicEnv } from "@/env/public";
 import { sdk } from "@farcaster/miniapp-sdk";
-import { useMutation, UseMutationOptions } from "@tanstack/react-query";
+import { useMutation, type UseMutationOptions } from "@tanstack/react-query";
 import { z } from "zod";
 import { useMiniAppContext } from "./useMiniAppContext";
+import { useRemoveChannelFromDiscoverQuery } from "@/queries/discover-channels";
+import { useUpdateChannelInChannelQuery } from "@/queries/channel";
+import { toast } from "sonner";
+import type { Channel } from "@/api/schemas";
 
 export class AlreadySubscribedError extends Error {
   constructor(message: string) {
@@ -19,14 +23,19 @@ const responseSchema = z.object({
 type Response = z.infer<typeof responseSchema>;
 
 type UseSubscribeToChannelOptions = {
-  channelId: bigint;
-} & Omit<UseMutationOptions<Response, Error, void>, "mutationFn">;
+  channel: Channel;
+} & Omit<
+  UseMutationOptions<Response, Error, void>,
+  "mutationFn" | "onSuccess" | "onError"
+>;
 
 export function useSubscribeToChannel({
-  channelId,
+  channel,
   ...options
 }: UseSubscribeToChannelOptions) {
   const miniAppContext = useMiniAppContext();
+  const updateChannelInChannelQuery = useUpdateChannelInChannelQuery();
+  const removeChannelFromDiscoverQuery = useRemoveChannelFromDiscoverQuery();
 
   return useMutation({
     ...options,
@@ -40,7 +49,7 @@ export function useSubscribeToChannel({
       }
 
       const url = new URL(
-        `/api/channels/${channelId}/subscribe`,
+        `/api/channels/${channel.id}/subscribe`,
         publicEnv.NEXT_PUBLIC_BROADCAST_APP_INDEXER_URL,
       );
 
@@ -54,14 +63,16 @@ export function useSubscribeToChannel({
 
       if (response.status === 409) {
         throw new AlreadySubscribedError(
-          `You are already subscribed to channel ${channelId}`,
+          `You are already subscribed to channel ${channel.name}`,
         );
       }
 
       if (!response.ok) {
-        throw new Error(
+        console.error(
           `Failed to subscribe to channel:\n\nStatus: ${response.status}\n\nResponse: ${await response.text()}`,
         );
+
+        throw new Error("Server returned invalid response");
       }
 
       const responseData = await responseSchema
@@ -69,6 +80,23 @@ export function useSubscribeToChannel({
         .parse(response.json());
 
       return responseData;
+    },
+    onError(error) {
+      if (error instanceof AlreadySubscribedError) {
+        toast.error(`You are already subscribed to channel ${channel.name}`);
+      } else {
+        toast.error(`Failed to subscribe to channel: ${error.message}`);
+      }
+    },
+    onSuccess(data) {
+      toast.success(`Subscribed to channel ${channel.name}`);
+
+      removeChannelFromDiscoverQuery(channel.id);
+
+      updateChannelInChannelQuery(channel.id, {
+        isSubscribed: true,
+        notificationsEnabled: data.notificationsEnabled,
+      });
     },
   });
 }
