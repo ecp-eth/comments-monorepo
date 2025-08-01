87bf9b63c9974697c1ef2a56869618cc40013c79
michalkvasnicak <michal.kvasnicak@gmail.com>
2025-08-01 16:15:45 +0200

    fix(indexer): vitest config

--- apps/indexer/vitest.config.ts
+++ apps/indexer/vitest.config.ts
@@ -9,10 +9,5 @@ process.env.ENS_RPC_URL ||= "https://ethereum-rpc.publicnode.com";
 process.env.SIM_API_KEY ||= "test";
 
 export default defineConfig({
-  plugins: [
-    // @ts-expect-error - ponder has outdated vite + vitest so we have conflicts
-    tsconfigPaths({
-      skip: (dir) => dir.includes("demo-rn-expo"),
-    }),
-  ],
+  plugins: [tsconfigPaths()],
 });
