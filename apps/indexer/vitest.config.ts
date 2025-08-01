cd23e83ae76964ca442194d7bf715c52bdec0d25
michalkvasnicak <michal.kvasnicak@gmail.com>
2025-08-01 16:15:45 +0200

    fix(indexer): vitest config

--- apps/indexer/vitest.config.ts
+++ apps/indexer/vitest.config.ts
@@ -2,10 +2,5 @@ import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    // @ts-expect-error - ponder has outdated vite + vitest so we have conflicts
-    tsconfigPaths({
-      skip: (dir) => dir.includes("demo-rn-expo"),
-    }),
-  ],
+  plugins: [tsconfigPaths()],
 });
