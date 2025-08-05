69c8a308eab2a2d8dc0c616caf3123c8a4838b67
michalkvasnicak <michal.kvasnicak@gmail.com>
2025-08-05 10:50:54 +0200

    chore(dependencies): update vitest to version 3.2.4 and @neynar/nodejs-sdk to version 2.44.0; adjust vitest config for compatibility

--- apps/indexer/vitest.config.ts
+++ apps/indexer/vitest.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    // @ts-expect-error - something incompatible regarding the types
+    tsconfigPaths({
+      skip: (dir) => dir.includes("demo-rn-expo"),
+    }),
+  ],
 });
