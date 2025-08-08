f151f31b60bf16354d762c1b974d41129fa72b41
Norm X <hi@norm.im>
2025-08-08 10:07:24 +0100

    chore: rename `channels-and-hooks` to be more generic (#518)
    * chore: rename channels-and-hoooks to be more generic
    
    
    
    * fix: paths

---
+++ examples/snippets/typescript/fetch-comments-without-moderation.ts
@@ -0,0 +1,16 @@
+import { fetchComments } from "@ecp.eth/sdk/indexer";
+
+/**
+ * This example shows how to fetch comments without moderation.
+ * This is useful if you want to display comments immediately without waiting for moderation approval.
+ */
+async function main() {
+  const comments = await fetchComments({
+    moderationStatus: ["approved", "pending"],
+    chainId: 8453,
+  });
+
+  console.log("Comments:", comments);
+}
+
+main();
