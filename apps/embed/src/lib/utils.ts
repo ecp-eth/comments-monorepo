5f08c6318111d0d5bc064290f489cd8d1dba9cd8
Norman Xu <hi@norm.im>
2025-08-13 14:16:51 +0100

    fix: should not use input type for values parsed

--- apps/embed/src/lib/utils.ts
+++ apps/embed/src/lib/utils.ts
@@ -3,7 +3,8 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { formatAuthorLinkWithTemplate } from "@ecp.eth/shared/helpers";
 import { AuthorType } from "@ecp.eth/shared/types";
-import { EmbedConfigSchemaInputType } from "@ecp.eth/sdk/embed/schemas";
+import { EmbedConfigSchema } from "@ecp.eth/sdk/embed/schemas";
+import z from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -16,7 +17,9 @@ export function formatAuthorLink(author: AuthorType): string | null {
   );
 }
 
-export function getAppSignerAddress(app: EmbedConfigSchemaInputType["app"]) {
+export function getAppSignerAddress(
+  app: z.output<typeof EmbedConfigSchema>["app"],
+) {
   return app === "embed"
     ? publicEnv.NEXT_PUBLIC_APP_SIGNER_ADDRESS
     : app === "all"
