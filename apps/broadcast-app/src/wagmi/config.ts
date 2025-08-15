bbbff2d143890c90913cca949d5dc4d8721ccf7b
michalkvasnicak <michal.kvasnicak@gmail.com>
2025-08-15 12:32:28 +0200

    feat(broadcast-app): update environment variables for ECP integration and add whitelist account script

--- apps/broadcast-app/src/wagmi/config.ts
+++ apps/broadcast-app/src/wagmi/config.ts
@@ -27,9 +27,11 @@ export const miniAppConfig = createConfig({
 });
 
 export const COMMENT_MANAGER_ADDRESS =
+  publicEnv.NEXT_PUBLIC_ECP_COMMENT_MANAGER_ADDRESS_OVERRIDE ||
   SUPPORTED_CHAINS[chain.id as keyof typeof SUPPORTED_CHAINS]
     .commentManagerAddress;
 
 export const CHANNEL_MANAGER_ADDRESS =
+  publicEnv.NEXT_PUBLIC_ECP_CHANNEL_MANAGER_ADDRESS_OVERRIDE ||
   SUPPORTED_CHAINS[chain.id as keyof typeof SUPPORTED_CHAINS]
     .channelManagerAddress;
