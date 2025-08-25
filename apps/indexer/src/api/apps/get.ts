5f589da07caf302e0596c86b738c7e54a02a8393
michalkvasnicak <michal.kvasnicak@gmail.com>
2025-08-25 09:50:15 +0200

    feat(indexer): add endpoint to retrieve list of apps with pagination support

---
+++ apps/indexer/src/api/apps/get.ts
@@ -0,0 +1,84 @@
+import { z, type OpenAPIHono } from "@hono/zod-openapi";
+import {
+  APIErrorResponseSchema,
+  OpenAPIDateStringSchema,
+} from "../../lib/schemas";
+import { appManager, siweMiddleware } from "../../services";
+
+export const AppGetRequestQuerySchema = z.object({
+  page: z.coerce.number().int().positive().default(1),
+  limit: z.coerce.number().int().positive().max(100).default(10),
+});
+
+const AppSchema = z.object({
+  id: z.string().uuid(),
+  createdAt: OpenAPIDateStringSchema,
+  updatedAt: OpenAPIDateStringSchema,
+  name: z.string().nonempty().max(50),
+});
+
+export const AppsGetResponseSchema = z.object({
+  results: z.array(AppSchema),
+  pageInfo: z.object({
+    totalPages: z.number().int().nonnegative(),
+  }),
+});
+
+export function setupAppsGet(app: OpenAPIHono) {
+  app.openapi(
+    {
+      method: "get",
+      path: "/apps",
+      tags: ["apps", "webhooks"],
+      middleware: siweMiddleware,
+      request: {
+        query: AppGetRequestQuerySchema,
+      },
+      responses: {
+        200: {
+          description: "Apps",
+          content: {
+            "application/json": {
+              schema: AppsGetResponseSchema,
+            },
+          },
+        },
+        400: {
+          description: "Invalid request",
+          content: {
+            "application/json": {
+              schema: APIErrorResponseSchema,
+            },
+          },
+        },
+        401: {
+          description: "Unauthorized",
+          content: {
+            "application/json": {
+              schema: APIErrorResponseSchema,
+            },
+          },
+        },
+        500: {
+          description: "Internal server error",
+          content: {
+            "application/json": {
+              schema: APIErrorResponseSchema,
+            },
+          },
+        },
+      },
+    },
+    async (c) => {
+      const { page, limit } = c.req.valid("query");
+
+      const result = await appManager.listApps({
+        page,
+        limit,
+        ownerId: c.get("user").id,
+      });
+
+      return c.json(AppsGetResponseSchema.parse(result), 200);
+    },
+  );
+}
