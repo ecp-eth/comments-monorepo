6af1a32748895666c27ae78065ce769be6dd23d3
michalkvasnicak <michal.kvasnicak@gmail.com>
2025-09-02 08:45:08 +0200

    refactor(indexer): simplify API URL creation by introducing createFetchUrl utility function

--- apps/indexer-app/src/lib/utils.ts
+++ apps/indexer-app/src/lib/utils.ts
@@ -1,6 +1,11 @@
+import { publicEnv } from "@/env/public";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function createFetchUrl(path: string): URL {
+  return new URL(path, publicEnv.NEXT_PUBLIC_INDEXER_URL);
 }
