aeeee49aa22fe305c0aff56bc9f50b9912476951
Norman Xu <hi@norm.im>
2025-09-10 16:34:09 +0100

    fix(demo-rn-expo): make targetUri and app signer config optional and add error screen

--- examples/demo-rn-expo/components/CommentSection.tsx
+++ examples/demo-rn-expo/components/CommentSection.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { fetchComments } from "@ecp.eth/sdk/indexer";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import {
@@ -7,6 +7,7 @@ import {
   ActivityIndicator,
   FlatList,
   Dimensions,
+  ScrollView,
 } from "react-native";
 import {
   useAnimatedStyle,
@@ -25,6 +26,7 @@ import { useOptimisticCommentingManager } from "../hooks/useOptimisticCommenting
 import { useDeleteComment } from "../hooks/useDeleteComment";
 import { chain } from "../wagmi.config";
 import { COMMENT_TYPE_COMMENT } from "@ecp.eth/sdk";
+import LinkButton from "../ui/LinkButton";
 
 type CommentSectionProps = {
   onReply: (comment: IndexerAPICommentSchemaType) => void;
@@ -45,49 +47,96 @@ export function CommentSection({
   const { repliesSectionAnimatedStyle, handleCloseReplies, handleViewReplies } =
     useRepliesAnimation(onViewReplies, onCloseViewReplies, replyingComment);
 
-  const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useInfiniteQuery({
-      queryKey: ["comments"],
-      initialPageParam: {
-        cursor: undefined as Hex | undefined,
-        // assuming comment box minimal height is 120, we want to at least fetch enough
-        // to fill the screen
-        // cursor: pageParam,
-        limit: Math.ceil(
-          Dimensions.get("window").height / COMMENT_BOX_AVERAGE_HEIGHT,
-        ),
-      },
-      queryFn: ({ pageParam, signal }) => {
-        return fetchComments({
-          apiUrl: publicEnv.EXPO_PUBLIC_INDEXER_URL,
-          targetUri: publicEnv.EXPO_PUBLIC_TARGET_URI,
-          app: publicEnv.EXPO_PUBLIC_APP_SIGNER_ADDRESS,
-          chainId: chain.id,
-          limit: pageParam.limit,
-          cursor: pageParam.cursor,
-          signal,
-          commentType: COMMENT_TYPE_COMMENT,
-        });
-      },
-      getNextPageParam: (lastPage) => {
-        if (!lastPage.pagination.hasNext) {
-          return;
-        }
+  const {
+    data,
+    isPending,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    error,
+    refetch,
+  } = useInfiniteQuery({
+    queryKey: ["comments"],
+    initialPageParam: {
+      cursor: undefined as Hex | undefined,
+      // assuming comment box minimal height is 120, we want to at least fetch enough
+      // to fill the screen
+      // cursor: pageParam,
+      limit: Math.ceil(
+        Dimensions.get("window").height / COMMENT_BOX_AVERAGE_HEIGHT,
+      ),
+    },
+    queryFn: ({ pageParam, signal }) => {
+      return fetchComments({
+        apiUrl: publicEnv.EXPO_PUBLIC_INDEXER_URL,
+        targetUri: publicEnv.EXPO_PUBLIC_TARGET_URI,
+        app: publicEnv.EXPO_PUBLIC_APP_SIGNER_ADDRESS,
+        chainId: chain.id,
+        limit: pageParam.limit,
+        cursor: pageParam.cursor,
+        signal,
+        commentType: COMMENT_TYPE_COMMENT,
+      });
+    },
+    getNextPageParam: (lastPage) => {
+      if (!lastPage.pagination.hasNext) {
+        return;
+      }
 
-        return {
-          cursor: lastPage.pagination.endCursor,
-          limit: lastPage.pagination.limit,
-        };
-      },
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
-      enabled: true,
-    });
+      return {
+        cursor: lastPage.pagination.endCursor,
+        limit: lastPage.pagination.limit,
+      };
+    },
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    enabled: true,
+  });
 
   const deletePendingOperations = useDeletePendingOperations(rootComment);
   const { mutateAsync: deleteComment } = useDeleteComment();
+  const [showErrorDetails, setShowErrorDetails] = useState(false);
 
-  if (isLoading) {
+  if (error) {
+    return (
+      <CommentSectionContainer>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          <Text>😶‍🌫️ Error fetching comments</Text>
+          <View style={{ marginVertical: 20, gap: 10, alignItems: "center" }}>
+            <LinkButton
+              onPress={() => {
+                refetch();
+              }}
+            >
+              Retry
+            </LinkButton>
+            <LinkButton
+              onPress={() => {
+                setShowErrorDetails(!showErrorDetails);
+              }}
+            >
+              {showErrorDetails ? "Hide error" : "Show error"}
+            </LinkButton>
+          </View>
+
+          {showErrorDetails && (
+            <ScrollView>
+              <Text>{error.message}</Text>
+            </ScrollView>
+          )}
+        </View>
+      </CommentSectionContainer>
+    );
+  }
+
+  if (isPending) {
     return (
       <CommentSectionContainer>
         <ActivityIndicator />
