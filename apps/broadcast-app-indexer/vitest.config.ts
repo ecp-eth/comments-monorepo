29fc5b17ae193e5fa981479e2fe7530ba467a4cb
Norman Xu <hi@norm.im>
2025-09-16 23:09:50 +0100

    chore: sync docs

--- apps/broadcast-app-indexer/vitest.config.ts
+++ apps/broadcast-app-indexer/vitest.config.ts
@@ -3,7 +3,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    // @ts-expect-error - something incompatible regarding the types
     tsconfigPaths({
       skip: (dir) => dir.includes("demo-rn-expo"),
     }),
