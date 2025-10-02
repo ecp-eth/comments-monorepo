c604bf47f320c75f87e7efce39b6e4bf8b9e6d34
Norm X <hi@norm.im>
2025-10-02 17:21:10 +0100

    chore(docs, indexer): make ENS tests retry with longer timeout and enlarge the heap size for doc gen (#588)
    * fix(docs): heap size not enough
    
    * chore(tests): make ens tests retry with longer timeout

--- apps/indexer/tests/resolvers/ens-by-name-resolver.test.ts
+++ apps/indexer/tests/resolvers/ens-by-name-resolver.test.ts
@@ -6,29 +6,43 @@ const resolver = createENSByNameResolver({
 });
 
 describe("ENSByNameResolver", () => {
-  it("should resolve ens name", async () => {
-    const result = await resolver.load("furlong.eth");
+  it(
+    "should resolve ens name",
+    async () => {
+      const result = await resolver.load("furlong.eth");
 
-    expect(result).toEqual({
-      address: expect.stringMatching(/^0x[0-9a-fA-F]{40}$/),
-      name: "furlong.eth",
-      avatarUrl: expect.toBeOneOf([null, expect.any(String)]),
-      url: expect.stringMatching(
-        /^https:\/\/app\.ens\.domains\/0x[0-9a-fA-F]{40}$/,
-      ),
-    });
-  });
+      expect(result).toEqual({
+        address: expect.stringMatching(/^0x[0-9a-fA-F]{40}$/),
+        name: "furlong.eth",
+        avatarUrl: expect.toBeOneOf([null, expect.any(String)]),
+        url: expect.stringMatching(
+          /^https:\/\/app\.ens\.domains\/0x[0-9a-fA-F]{40}$/,
+        ),
+      });
+    },
+    {
+      retry: 3,
+      timeout: 10000,
+    },
+  );
 
-  it("should resolve base ens name", async () => {
-    const result = await resolver.load("davidf.base.eth");
+  it(
+    "should resolve base ens name",
+    async () => {
+      const result = await resolver.load("davidf.base.eth");
 
-    expect(result).toEqual({
-      address: expect.stringMatching(/^0x[0-9a-fA-F]{40}$/),
-      name: "davidf.base.eth",
-      avatarUrl: expect.toBeOneOf([null, expect.any(String)]),
-      url: expect.stringMatching(
-        /^https:\/\/app\.ens\.domains\/0x[0-9a-fA-F]{40}$/,
-      ),
-    });
-  });
+      expect(result).toEqual({
+        address: expect.stringMatching(/^0x[0-9a-fA-F]{40}$/),
+        name: "davidf.base.eth",
+        avatarUrl: expect.toBeOneOf([null, expect.any(String)]),
+        url: expect.stringMatching(
+          /^https:\/\/app\.ens\.domains\/0x[0-9a-fA-F]{40}$/,
+        ),
+      });
+    },
+    {
+      retry: 3,
+      timeout: 10000,
+    },
+  );
 });
