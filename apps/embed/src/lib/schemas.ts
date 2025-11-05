31c25d44063b2abf3ae7b71c87828567efd559af
Norman Xu <hi@norm.im>
2025-11-05 09:47:26 +0000

    fixes

--- apps/embed/src/lib/schemas.ts
+++ apps/embed/src/lib/schemas.ts
@@ -1,4 +1,6 @@
 import { z } from "zod";
+import { decompressFromURI } from "lz-ts";
+import { EmbedConfigSchema } from "@ecp.eth/sdk/embed/schemas";
 
 export const BadRequestResponseSchema = z.record(
   z.string(),
@@ -16,3 +18,16 @@ export const GenerateUploadUrlResponseSchema = z.object({
 export type GenerateUploadUrlResponseSchemaType = z.infer<
   typeof GenerateUploadUrlResponseSchema
 >;
+
+/**
+ * Embed config from search params
+ */
+export const EmbedConfigFromSearchParamsSchema = z.preprocess((value) => {
+  try {
+    if (typeof value === "string") {
+      return JSON.parse(decompressFromURI(value));
+    }
+  } catch (err) {
+    console.warn("failed to parse config", err);
+  }
+}, EmbedConfigSchema.default({}));
